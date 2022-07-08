43835ef4aca5c29dfa643895653c80f640070b7b
httpkevin <michaelkevinmendes@gmail.com>
2022-07-07 23:59:56 -0300

    add function de responsividade

---
+++ src/index.js
@@ -0,0 +1,18 @@
+//
+let x, y, z;
+
+x = document.getElementById("login");
+y = document.getElementById("register");
+z = document.getElementById("btn");
+
+function register() {
+    x.style.left = "-400px";
+    y.style.left = "50px";
+    z.style.left = "110px";
+}
+
+function login() {
+    x.style.left = "50px";
+    y.style.left = "450px";
+    z.style.left = "0px";
+}
