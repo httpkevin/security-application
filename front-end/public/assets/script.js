dd41844a2dd135a74403a3900081367bda91951d
httpkevin <michaelkevinmendes@gmail.com>
2022-07-27 21:30:56 -0300

    function de relogio em real time

--- front-end/public/assets/script.js
+++ front-end/public/assets/script.js
@@ -64,3 +64,32 @@ menu_item.forEach((item) => {
 	});
 });
 
+// Função para ver hora em tempo real 
+setInterval(function() {
+
+    let hoursnew = new Date();
+    let hours = hoursnew.getHours();
+    let minutes = hoursnew.getMinutes();
+    let seconds = hoursnew.getSeconds();
+
+    // Chamamos a função zero para que ela retorne a concatenação
+    // com os minutos e segundos
+    minutes = zero(minutes);
+    seconds = zero(seconds);
+
+    // Com o textContent, iremos inserir as horas, minutos e segundos
+    // no nosso elemento HTML
+    document.getElementById("hours").textContent = hours+':'+minutes+':'+seconds;
+},1000)
+
+// A function zero concatena a string (número) 0 em frente aos números
+// mantendo o zero na frente dos números menores que 10. Exemplo:
+// 21:05:01
+// 21:05:02
+// e assim, sucessivamente
+function zero(x) {
+    if (x < 10) {
+        x = '0' + x;
+    } return x;
+}
+
